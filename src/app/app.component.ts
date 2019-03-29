b3f9af8b1c4bb1c2b620f8d13e5b838885a7c042
javnet <julioansede@gmail.com>
2019-03-29 03:45:04 +0100

    [ADD] Initial Styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'pr-root',
+  selector: 'sn-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
